78adbf74c9eddb0fe75045116381abe912f672fe
Carlos Encine <contato@carlosencine.com>
2020-05-14 17:17:00 -0300

    change namespace of api

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::namespace('api')->group(function () {
+Route::namespace('Api')->group(function () {
     Route::prefix('countries')->group(function () {
         Route::get('/', 'CountryController@index')->name('country.main');
         Route::post('/', 'CountryController@store')->name('country.store');
